152c93a6beeb1c2bd06eaeb83c4c432c1004494d
taissadames <taissadames@Marlons-MacBook-Pro.local>
2022-10-27 22:08:50 +1030

    updated

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,20 +1,20 @@
-import i18n from "i18next"
-import {initReactI18next} from "react-i18next"
+// import i18n from "i18next"
+// import {initReactI18next} from "react-i18next"
 
-import enJson from './translations/en.json'
-import esJson from './translations/es.json'
-import ptJson from './translations/pt.json'
+// import enJson from './translations/en.json'
+// import esJson from './translations/es.json'
+// import ptJson from './translations/pt.json'
 
-i18n.use(initReactI18next).init({
-  fallbackLng: "en",
-  interpolation: {
-    escapeValue: false
-  },
-  resources: {
-    en: enJson,
-    es: esJson,
-    pt: ptJson,
-  },
-})
+// i18n.use(initReactI18next).init({
+//   fallbackLng: "en",
+//   interpolation: {
+//     escapeValue: false
+//   },
+//   resources: {
+//     en: enJson,
+//     es: esJson,
+//     pt: ptJson,
+//   },
+// })
 
-export default i18n;
+// export default i18n;
